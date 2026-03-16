b7a1c8707ea6fa940e5b311aa0f065dd2cf48d70
aman <amansantoshvarmabsc24@rvu.edu.in>
2026-03-16 12:55:02 +0530

    Add files via upload

---
+++ integrity.py
@@ -0,0 +1,14 @@
+<<<<<<< HEAD
+string = input("Enter a string: ")
+
+if string == string[::-1]:
+    print("Data hasnt been corrupted")
+else:
+=======
+string = input("Enter a string: ")
+
+if string == string[::-1]:
+    print("Data hasnt been corrupted")
+else:
+>>>>>>> 6d0103f6ffc048688165709369fea326e0bafc31
+    print("Data corrupted")
